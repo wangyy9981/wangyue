d873b8e9effee5c56465a6760f528ed32c2433ca
waangyy9981 <1344366870@qq.comm>
2020-10-28 14:02:45 +0800

    10.28

---
+++ vue-meitaun/src/views/Home/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Head></Head>
+        <Category></Category>
+        <List></List>
+    </div>
+</template>
+
+<script>
+    import Head from './Head'
+    import List from './List'
+    import Category from './Category'
+
+    export default {
+        components:{
+            Head,
+            Category,
+            List
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
